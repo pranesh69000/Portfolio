2f18f4a16e686f777c5278948c32944d556ebd02
PRANESH R <pranesh.2305117@srec.ac.in>
2025-06-29 05:17:37 +0530

    portfolio

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Welcome to my PC setup! These days, I’m diving deep into the MERN stack, building cool projects and leveling up as a Full-Stack Developer. I’ve also got a soft spot for Java and Python . I just love coding, no matter the language. Wanna peek into my world? Check out my <a href="https://github.com/pranesh69000" target="_blank">Github</a>!`,
+
+    "cs-degree": `Currently pursuing my B.Tech in Information Technology at Sri Ramakrishna Engineering College. It’s been the perfect blend of theory and hands-on experience, giving me a solid launchpad into the world of tech.`,
+    
+    "sofa-table": `This is my chill zone. After college, I crash here to scroll through LinkedIn, stay updated with the latest in tech, or drop a few posts of my own. I’m also big on learning through YouTube tutorials and other online platforms. Wanna connect? Hit up <a href="https://www.linkedin.com/in/praneshchandran21/" target="_blank">My LinkedIn</a>!`,
+    
+    tv: `Yep, that’s my TV , lately it’s been all about tech YouTubers! I’m hooked on <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo (t3.gg)</a>, <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> occasionally, and of course <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>. These creators seriously inspire me to build more and code better!`,
+    
+    bed: `This is where the overthinking happens 😅. Sometimes I dream big, sometimes I spiral but hey, that’s the creative brain for you. Eventually, I knock out and recharge for another day of building and learning.`,
+    
+    resume: `Here’s my workspace and  right on that desk  is my resume! <a href="https://drive.google.com/file/d/19A5jw9VTOFsIgFr5KS38Rmlq7u6LaOQZ/view?usp=drive_link" target="_blank">Check it out?</a> If you’re hiring and looking for someone who’s driven, creative and all about growth, shoot me an email at praneshchandranwork@gmail.com. I’m ready to contribute and grow with your team!`,
+    
+    projects: `A little behind-the-scenes: this interactive portfolio is powered by Kaboom.js , a super cool game development library in JavaScript. The UI magic comes from HTML and CSS. I wanted to build something that’s *me* , creative, fun and code-powered.`,
+    
+    library: `Here’s my mini tech library! Let’s be real , I’ve probably read one of these cover to cover 😅. But hey, collecting dev books is a personality trait, right? The hunger to learn is always there!`,
+    
+    exit: `Want to leave Pranesh R’s portfolio? Just close the tab  but hey, don’t go too soon. There’s still more awesome to explore! 😉`,
+    
+};
